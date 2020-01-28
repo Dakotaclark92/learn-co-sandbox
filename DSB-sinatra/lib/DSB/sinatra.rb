8b1ccbb1cdb3de5737a68d1fcecadc0ee2a36a2e
Dakota Clark <dakotaclark92@yahoo.com>
2020-01-28 01:12:17 +0000

    adding character creation

--- DSB-sinatra/lib/DSB/sinatra.rb
+++
@@ -1,8 +0,0 @@
-require "DSB/sinatra/version"
-
-module DSB
-  module Sinatra
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- DSB-sinatra/lib/DSB/sinatra/version.rb
+++
@@ -1,5 +0,0 @@
-module DSB
-  module Sinatra
-    VERSION = "0.1.0"
-  end
-end
